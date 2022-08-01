13301d66234b4bc210f3c7376e751dc3b0c9e33f
Isiwara Koralage <87434644+IsiwaraK@users.noreply.github.com>
2022-08-01 20:29:25 +0530

    navigation css

--- job/frontend/src/components/header.jsx
+++ job/frontend/src/components/header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from '../images/logo.png';
+
 import '../CSS/header.css'
 
 export default function header() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <a className="navbar-brand text-white" href="#">
-          MATRIX
-        </a>
+      <div className="logo">
+            <img className="img1" src={logo} /></div>
 
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
