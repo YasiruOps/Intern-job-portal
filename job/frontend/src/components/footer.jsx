d8572fe634bcbd9389e90aa7e71e1f8a1cadb735
Isiwara Koralage <87434644+IsiwaraK@users.noreply.github.com>
2022-08-08 17:46:08 +0530

    footer

--- job/frontend/src/components/footer.jsx
+++ job/frontend/src/components/footer.jsx
@@ -1,15 +1,106 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from '../images/logo.png';
+import logo1 from '../images/logo.png';
 import {Col,Row}  from 'react-bootstrap';
 import '../CSS/footer.css'
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
 
 
 export default function footer() {
   return (
-    <div className="footer">
-    <p>This is some content in sticky footer</p>
-  </div>
+    <MDBFooter className='footer-header text-lg-start text-muted'>
+    <section className='d-flex justify-content-lg-between p-4'>
+      <div className='footer-header1 me-5 d-none d-lg-block'>
+        <div className='footertopic'><span>About Us</span></div>
+      </div>
+    </section>
+
+    <section className='foot'>
+      <MDBContainer className='footer-container text-md-start mt-5'>
+        <MDBRow className='mt-3'>
+
+          <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+          <img className="img2" src={logo1} width = "220" height = "auto" />
+            <p>
+              Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit.
+            </p>
+          </MDBCol>
+          
+          <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+            <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+            <p>
+              <a href='#!' className='text-reset'>
+                Angular
+              </a>
+            </p>
+            <p>
+              <a href='#!' className='text-reset'>
+                React
+              </a>
+            </p>
+            <p>
+              <a href='#!' className='text-reset'>
+                Vue
+              </a>
+            </p>
+            <p>
+              <a href='#!' className='text-reset'>
+                Laravel
+              </a>
+            </p>
+          </MDBCol>
+
+          <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
+            <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+            <p>
+              <a href='#!' className='text-reset'>
+                Pricing
+              </a>
+            </p>
+            <p>
+              <a href='#!' className='text-reset'>
+                Settings
+              </a>
+            </p>
+            <p>
+              <a href='#!' className='text-reset'>
+                Orders
+              </a>
+            </p>
+            <p>
+              <a href='#!' className='text-reset'>
+                Help
+              </a>
+            </p>
+          </MDBCol>
+
+          <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
+            <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+            <p>
+              <MDBIcon icon="home" className="me-2" />
+              New York, NY 10012, US
+            </p>
+            <p>
+              <MDBIcon icon="envelope" className="me-3" />
+              info@example.com
+            </p>
+            <p>
+              <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
+            </p>
+            <p>
+              <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
+            </p>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      
+    </section>
+
+    <div className='footer-bottom p-4'>
+      © 2021 Matrix.  All Rights Reserved
+    </div>
+  </MDBFooter>
     
   );
 }
